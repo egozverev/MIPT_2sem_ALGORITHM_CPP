6a13b6df9a195ef5c8cf0d8551f7e06dfabcb749
egozverev <37687005+egozverev@users.noreply.github.com>
2019-03-04 12:47:37 +0300

    Update hash_graph.h

--- Module1(Graphs)/Task4/hash_graph.h
+++ Module1(Graphs)/Task4/hash_graph.h
@@ -1,15 +1,12 @@
 #pragma once
 
-#include "graph.h"
-
 #include <unordered_set>
 #include <vector>
 using std::vector;
 using std::unordered_set;
-class CHashGraph : public IGraph {
+class CHashGraph{
 public:
     CHashGraph(int vertexCount);
-    CHashGraph(const IGraph* _graph);
     virtual void AddEdge(int from, int to);
     virtual int VerticesCount() const;
     virtual void GetNextVertices(int vertex, vector<int>& vertices) const;
