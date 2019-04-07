6fa4de16d46706adaf0b5d0fda2b9dae2eddecb5
egozverev <zzwerruga@mail.ru>
2019-04-07 14:07:42 +0300

    Lots of little fixes

--- Module2/Task6/main.cpp
+++ Module2/Task6/main.cpp
@@ -1,38 +1,41 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
-void InputSquareMatrix(std::vector<std::vector<int>> &matrix, int order) {
-    for (int first = 0; first < order; ++first) {
-        for (int second = 0; second < order; ++second) {
+
+void InputSquareMatrix(std::vector<std::vector<int>> &matrix) {
+    for (size_t first = 0; first < matrix.size(); ++first) {
+        for (size_t second = 0; second < matrix.size(); ++second) {
             std::cin >> matrix[first][second];
         }
     }
 }
 
-void Floid(std::vector<std::vector<int>> &adjacencyMatrix) {
-    for (int stage = 0; stage < adjacencyMatrix.size(); ++stage) {
-        for (int first = 0; first < adjacencyMatrix.size(); ++first) {
-            for (int second = 0; second < adjacencyMatrix[0].size(); ++second) {
-                adjacencyMatrix[first][second]=std::min(adjacencyMatrix[first][second], adjacencyMatrix[first][stage]+adjacencyMatrix[stage][second]);
+void CalculateShortestPaths(std::vector<std::vector<int>> &adjacencyMatrix) {
+    for (size_t i = 0; i < adjacencyMatrix.size(); ++i) {
+        for (size_t first = 0; first < adjacencyMatrix.size(); ++first) {
+            for (size_t second = 0; second < adjacencyMatrix.size(); ++second) {
+                int oldPath = adjacencyMatrix[first][second];
+                int newPath = adjacencyMatrix[first][i] + adjacencyMatrix[i][second];
+                adjacencyMatrix[first][second] = std::min(oldPath, newPath);
             }
         }
     }
 }
-void PrintMatrix(std::vector<std::vector<int>> &matrix){
-    for(int first=0; first< matrix.size();++first){
-        for(int second=0; second<matrix[0].size(); ++second){
-            std::cout<<matrix[first][second]<<" ";
+
+void PrintMatrix(const std::vector<std::vector<int>> &matrix) {
+    for (auto first: matrix) {
+        for (auto second: first) {
+            std::cout << second << " ";
         }
-        std::cout<<"\n";
+        std::cout << std::endl;
     }
 }
+
 int main() {
     unsigned int n;
     std::cin >> n;
-    std::vector<std::vector<int>> adjacencyMatrix(n, std::vector<int>(n, 0));
-    InputSquareMatrix(adjacencyMatrix, n);
-    Floid(adjacencyMatrix);
+    std::vector<std::vector<int>> adjacencyMatrix(n, std::vector<int>(n));
+    InputSquareMatrix(adjacencyMatrix);
+    CalculateShortestPaths(adjacencyMatrix);
     PrintMatrix(adjacencyMatrix);
-
-
 }
