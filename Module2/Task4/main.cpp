4c35788ea4c2fcf7a989900477e99dbd2b82fe30
egozverev <zzwerruga@mail.ru>
2019-04-08 19:03:43 +0300

    Adding bitsets

--- Module2/Task4/main.cpp
+++ Module2/Task4/main.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <bitset>
 #include <iostream>
 #include <string>
 #include <queue>
@@ -5,108 +7,120 @@
 #include <unordered_map>
 #include <cmath>
 #include <functional>
-using std::string;
-using std::unordered_map;
-using std::pair;
-using std::priority_queue;
-using std::make_pair;
-using std::cin;
-using std::cout;
-using std::pair;
-using std::vector;
-enum Colors {
+using namespace std;
+enum Directions {
     Left = 'L',
     Right = 'R',
     Down = 'D',
     Up = 'U',
     None = 'N'
 };
-char ConvertIntToChar(int number) {
-    if (number <= 9) {
-        return static_cast<char>(number + 48); //convert num to 'num' correctly
-    } else {
-        return static_cast<char>(55 + number); //convert 10 to A, 11 to B,... 15 to F
-    } //Типа шестнадцатеричная система кодирования
+
+bitset<64> EncodeVectorToBitset(vector<int>& vec){
+    bitset<64> encoded;
+    int i=0;
+    for(int number: vec){
+        for(int j=3; j>=0; --j){
+            encoded[i+j] = number % 2;
+            number /= 2;
+        }
+        i += 4;
+    }
+    return encoded;
 }
 
+vector<int> DecodeVectorFromBitset(bitset<64>& encoded){
+    vector<int> decoded;
+    int i=0;
+    bitset<4> cur;
+    while(i<64){
+        cur[3-i%4] = encoded[i];
+        if(i%4==3){
+            decoded.push_back(static_cast<int>(cur.to_ulong()));
+        }
+        ++i;
 
-int ConvertCharToInt(char sym) {
-    if (sym < '9') {
-        return sym - 48;
-    } else {
-        return sym - 55;
     }
+    return decoded;
 }
 
 class Node {
 public:
-    Node():movement(None), parent(nullptr), position("0"){}
-    Node(Colors move,Node* parent_,const string& pos) : movement(move), parent(parent_), position(pos) {
+    Node():movement(None), parent(nullptr){}
+    Node(Directions move,Node* parent_,bitset<64> pos) : movement(move), parent(parent_), position(pos) {
     }
-    string position;
-    Colors movement; // U/D/R/L/N
+    Node(Directions move, Node* parent_, vector<int> pos ): movement(move), parent(parent_){
+        position = EncodeVectorToBitset(pos);
+    }
+    bitset<64> position;
+    Directions movement; // U/D/R/L/N
     Node* parent;
     int distanceFromRoot = 0;
 
     bool CanBeSolved();
-    Node CalculateMovedNode(Colors where);
+    Node CalculateMovedNode(Directions where);
     int FindZeroPosition();
     int Heuristics();
-private:
-    static const unsigned int size = 16;
 };
 int Node::FindZeroPosition(){
-    return position.find('0');
+    vector<int> vec =DecodeVectorFromBitset(position);
+    int cur =0;
+    while(cur<16 && vec[cur]!= 0){
+        ++cur;
+    }
+    return cur;
 }
 
 bool Node::CanBeSolved() {
+    const unsigned int size = 16;
     int inversionNum = 0;
+    vector<int> vecPos = DecodeVectorFromBitset(position);
     for (int first = 0; first < size; ++first) {
         for (int second = first + 1; second < size; ++second) {
-            if (position[first] == '0' || position[second] == '0') {
+            if (vecPos[first] == 0 || vecPos[second] == 0) {
                 continue;
             }
-            if (position[first] > position[second]) {
+            if (vecPos[first] > vecPos[second]) {
                 ++inversionNum;
             }
         }
     }
-    return (inversionNum + position.find('0') / 4) % 2;
+    return (inversionNum + FindZeroPosition() / 4) % 2 == 1;
 
 }
 
 int Node::Heuristics() {
     const unsigned char size = 16;
     int heuristics = 0;
+    vector<int> vecPos = DecodeVectorFromBitset(position);
     for (int first = 0; first < size; ++first) {
         int idealPos = !first ? size - 1 : first - 1;
-        int pos = position.find(ConvertIntToChar(first));
+        int pos = find(vecPos.begin(), vecPos.end(),first)- vecPos.begin();
         heuristics += abs(pos % 4 - idealPos % 4) + abs(pos / 4 - idealPos / 4);
 
     }
     for (int first = 0; first < size; ++first) {
         int idealPosFirst = !first ? size - 1 : first - 1;
-        int posFirst = position.find(ConvertIntToChar(first));
+        int posFirst = find(vecPos.begin(), vecPos.end(), first)-vecPos.begin();
         if (idealPosFirst / 4 == posFirst / 4) {
             for (int posSecond = (posFirst / 4) * 4; posSecond < posFirst; ++posSecond) {
-                int idealPosSecond = !ConvertCharToInt(position[posSecond]) ?
-                                     size - 1 : ConvertCharToInt(position[posSecond]) - 1;
+                int idealPosSecond = !vecPos[posSecond] ?
+                                     size - 1 : vecPos[posSecond] - 1;
                 if (idealPosSecond / 4 == posSecond / 4 &&(
-                        !ConvertCharToInt(position[posSecond]) || (
-                                ConvertCharToInt(position[posFirst]) &&
-                                position[posSecond] > position[posFirst])) ){
+                        !vecPos[posSecond] || (vecPos[posFirst] &&
+                                vecPos[posSecond] > vecPos[posFirst])) ){
                     heuristics += 2;
                 }
             }
         }
         if (idealPosFirst % 4 == posFirst % 4) {
             for (int posSecond = posFirst % 4; posSecond < posFirst; posSecond += 4) {
-                int idealPosSecond = !ConvertCharToInt(position[posSecond]) ?
-                                     size - 1 : ConvertCharToInt(position[posSecond]) - 1;
+                int idealPosSecond = !vecPos[posSecond] ?
+                                     size - 1 : vecPos[posSecond] - 1;
                 if (idealPosSecond %4 == posSecond % 4 &&(
-                        !ConvertCharToInt(position[posSecond]) || (
-                                ConvertCharToInt(position[posFirst]) &&
-                                position[posSecond] > position[posFirst])) ){
+                        !vecPos[posSecond] || (
+                                vecPos[posFirst] &&
+                                        vecPos[posSecond] > vecPos[posFirst])) ){
                     heuristics += 2;
                 }
             }
@@ -116,8 +130,8 @@ int Node::Heuristics() {
 
     return heuristics*117/80;
 }
-Node Node::CalculateMovedNode(Colors where){
-    string newPosition = position;
+Node Node::CalculateMovedNode(Directions where){
+    vector<int> newPosition = DecodeVectorFromBitset(position);
     int pos = FindZeroPosition();
     if (where == Left) {
         std::swap(newPosition[pos], newPosition[pos - 1]);
@@ -132,30 +146,36 @@ Node Node::CalculateMovedNode(Colors where){
 }
 
 
+class Compare {
+public:
+    bool operator()(pair<int, bitset<64>> first, pair<int, bitset<64>> second) {
+        return first.first > second.first;
+    }
+};
 
-
-bool FifteenGame(string firstPosition, string &answer) {
-
+bool FifteenGame(vector<int> firstVecPosition, string &answer) {
+    bitset<64> firstPosition = EncodeVectorToBitset(firstVecPosition);
+    vector<int> goalPositionVector {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0};
+    const bitset<64> goalPositionSet = EncodeVectorToBitset(goalPositionVector);
     Node firstNode(None, nullptr, firstPosition);
     if (!firstNode.CanBeSolved()) {
         return false;
     }
-
-    unordered_map<string, int> distances;
-    unordered_map<string, Node> nodeMap; // Храним использованные вершины ( инфу о них )
-    unordered_map<string, bool> wasUsed;
-    priority_queue<pair<int, string>, std::vector<pair<int, string>>, std::greater<>> heap; // pair<distance, string>
+    unordered_map<bitset<64>, int> distances;
+    unordered_map<bitset<64>, Node> nodeMap; // Храним использованные вершины ( инфу о них )
+    unordered_map<bitset<64>, bool> wasUsed;
+    priority_queue<pair<int, bitset<64>>, std::vector<pair<int, bitset<64>>>, Compare> heap; // pair<distance, string>
 
     heap.push(make_pair(firstNode.Heuristics(), firstPosition));
 
     distances[firstPosition] = firstNode.Heuristics();
     nodeMap.emplace(firstPosition, firstNode);
     while (!heap.empty()) {
-        pair<int, string> current = heap.top();
+        pair<int, bitset<64>> current = heap.top();
         heap.pop();
-        string currentState = current.second;
+        bitset<64> currentState = current.second;
         Node* currentNode = &nodeMap[currentState];
-        if (currentState == "123456789ABCDEF0") {
+        if (currentState == goalPositionSet) {
             string reversedAns;
             while (currentNode->position != firstPosition) {
                 reversedAns.push_back(currentNode->movement);
@@ -173,7 +193,7 @@ bool FifteenGame(string firstPosition, string &answer) {
         }
         wasUsed[currentState] = true;
         int zeroPosition = currentNode->FindZeroPosition();
-        vector<pair<Colors, Colors> > possibleMoves;
+        vector<pair<Directions, Directions> > possibleMoves;
         if (zeroPosition % 4 != 3){
             possibleMoves.emplace_back(make_pair(Right, Left));
         }
@@ -188,10 +208,9 @@ bool FifteenGame(string firstPosition, string &answer) {
         }
         for(auto move: possibleMoves){
             Node newNode = currentNode->CalculateMovedNode(move.first);
-            string newPosition = newNode.position;
-            if (!wasUsed[newPosition] && (!distances.count(newPosition) ||
-                                          distances[newPosition] >
-                                          nodeMap[currentState].distanceFromRoot + 1 + newNode.Heuristics())) {
+            bitset<64> newPosition = newNode.position;
+            int newDis = nodeMap[currentState].distanceFromRoot + 1 + newNode.Heuristics();
+            if (!wasUsed[newPosition] && (!distances.count(newPosition) || distances[newPosition] > newDis)) {
                 nodeMap[newPosition] = Node(move.second, currentNode , newPosition);
                 int newDistance = nodeMap[currentState].distanceFromRoot + 1;
                 nodeMap[newPosition].distanceFromRoot = newDistance;
@@ -208,11 +227,11 @@ bool FifteenGame(string firstPosition, string &answer) {
 
 int main() {
     const unsigned int size = 16;
-    string firstPosition;
+    vector<int> firstPosition;
     for (int i = 0; i < size; ++i) {
         int next;
         cin >> next;
-        firstPosition.push_back(ConvertIntToChar(next));
+        firstPosition.push_back(next);
     }
 
     string answer;
